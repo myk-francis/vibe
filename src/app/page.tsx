6d02ac98644d6a2fc286684c2a466efe7a140d59
myk <michaelmugendi1707@gmail.com>
2025-06-27 14:40:35 +0300

    05: ai jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,36 @@
 "use client";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useTRPC } from "@/trpc/client";
+import { useMutation } from "@tanstack/react-query";
 import React from "react";
+import { toast } from "sonner";
 
 const Page = () => {
-  return <div>Page</div>;
+  const [value, setValue] = React.useState("");
+  const trpc = useTRPC();
+  const invoke = useMutation(
+    trpc.invoke.mutationOptions({
+      onSuccess: () => {
+        toast.success("Invoked");
+      },
+    })
+  );
+  return (
+    <div className="p-4 max-w-7xl mx-auto">
+      <Input
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <Button
+        disabled={invoke.isPending}
+        onClick={() => invoke.mutate({ value })}
+      >
+        Invoke
+      </Button>
+    </div>
+  );
 };
 
 export default Page;
